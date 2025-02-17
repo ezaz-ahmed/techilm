fe431162311cc8173e568a2cdf0ab2879806eb8f
Ezaz Ahmed <ezaz@grype.ca>
2025-02-17 16:21:28 +0600

    ✨ feat: Initialize new project with Tailwind CSS, Prettier, and ESLint setup
    - Installed and configured Tailwind CSS for styling
    - Set up Prettier for code formatting
    - Integrated ESLint for code linting

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'NeoIlm - Learning Management System (LMS) for Seamless Education',
+  description:
+    'NeoIlm is an intuitive Learning Management System (LMS) designed to streamline educational experiences, enhance learning engagement, and simplify course management for educators and students alike.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
